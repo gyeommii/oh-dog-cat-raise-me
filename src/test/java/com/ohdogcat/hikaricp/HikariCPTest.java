75bba384a810297dc9e927b387dda0d024a736c3
gyeommii <yydasol@gmail.com>
2023-12-13 14:49:41 +0900

    test

---
+++ src/test/java/com/ohdogcat/hikaricp/HikariCPTest.java
@@ -0,0 +1,57 @@
+package com.ohdogcat.hikaricp;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+public class HikariCPTest {
+	private static final Logger log = LoggerFactory.getLogger(HikariCPTest.class);
+
+	@Test
+	public void test() throws SQLException {
+		// HikariCP(커넥션 풀) 환경 설정을 위한 객체 생성:
+		HikariConfig config = new HikariConfig();
+
+		// HikariCP 환경 설정:
+		config.setDriverClassName("oracle.jdbc.OracleDriver");
+		config.setJdbcUrl("jdbc:oracle:thin:@192.168.20.11:1521:xe");
+		config.setUsername("scott");
+		config.setPassword("tiger");
+
+		// 환경 설정 내용이 적용된 커넥션 풀 객체(DataSource)를 생성:
+		HikariDataSource ds = new HikariDataSource(config);
+
+		// 커넥션 풀(데이터 소스) 객체는 null이 아니어야 함.
+		Assertions.assertNotNull(ds);
+		log.debug("ds={}", ds);
+
+		// 커넥션 풀(데이터 소스)에서 커넥션 객체를 빌려옴.
+		Connection conn = ds.getConnection();
+
+		PreparedStatement stmt = conn.prepareStatement("INSERT INTO BLOGS (TITLE, CONTENT, AUTHOR) VALUES (?, ?, ?)");
+		stmt.setString(1, "hihi");
+		stmt.setString(2, "hello");
+		stmt.setString(3, "hola");
+
+		int result = stmt.executeUpdate();
+
+		Assertions.assertEquals(1, result);
+
+		// 커넥션 객체는 null이 아니어야 함.
+		Assertions.assertNotNull(conn);
+		log.debug("conn={}", conn);
+
+		// 사용이 끝난 커넥션 객체는 풀에 반환.
+		conn.close(); // close: DB 서버와의 물리적인 연결을 끊는게 아니라 커넥션 풀에 반환!
+		log.debug("커넥션 반환 성공");
+	}
+
+}
---
+++ src/test/java/com/ohdogcat/ojdbc/JdbcTest.java
@@ -0,0 +1,47 @@
+package com.ohdogcat.ojdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import oracle.jdbc.OracleDriver;
+
+/*
+ * JUnit 테스트(자바 단위 테스트)를 하기 위한 클래스 작성.
+ * main 메서드를 작성하지 않음.
+ * @Test 애너테이션을 사용한 메스드를 작성.
+ * Run As->JUnit Test를 실행하면, junit-jupiter-engine에서 테스트 메서드를 실행.
+ */
+public class JdbcTest {
+    private static final String URL = "jdbc:oracle:thin:@192.168.20.11:1521:xe";
+    private static final String USER = "scott";
+    private static final String PASSWORD = "tiger";
+    private static final Logger log = LoggerFactory.getLogger(JdbcTest.class);
+    
+    /*
+     * 테스트 메서드 작성:
+     * @Test 애너테이션을 사용.
+     * public void로 선언. 파라미터를 선언하지 않아야 함.
+     */
+    @Test
+    public void test() throws SQLException {
+        // 1. JDBC 라이브러리(드라이버)를 등록:
+        DriverManager.registerDriver(new OracleDriver());
+        log.debug("오라클 JDBC 드라이버 등록 성공");
+        
+        // 2. 데이터베이스와 Connection을 맺음:
+        Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+        // 개발자가 정상 결과라고 예상(expect)하는 값을 테스트:
+        Assertions.assertNotNull(conn);
+        log.debug("conn={}", conn);
+        
+        // 리소스 해제:
+        conn.close();
+    }
+
+}
