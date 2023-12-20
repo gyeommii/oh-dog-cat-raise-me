bba824cba692de5c30300423b4287e3f5ed956a5
gyeommii <yydasol@gmail.com>
2023-12-20 19:44:49 +0900

    mapper 수정

--- src/main/webapp/static/js/product-sorted.js
+++ src/main/webapp/static/js/product-sorted.js
@@ -30,32 +30,7 @@
         });
     });
     
-    function showProductsCards(products) {
-    var productsContainer = document.getElementById('productsContainer');
-    productsContainer.innerHTML = ''; // 상품목록 초기화
-                console.log(products);
-
-    if (!Array.isArray(products)) {
-        console.error('products는 배열이 아님:', products);
-        return; // 배열 아니면 종료
-    }
     
-
-    products.forEach(function (product) {
-        var cardHtml = `
-            <div class="col-3">
-            <div class="card mb-5">
-                <img class="card-img-top" src="${product.imgUrl}" alt="Product image">
-                <div class="card-body">
-                    <h5 class="card-title">${product.productName}</h5>
-                    <p class="card-text">${product.minPrice}</p>
-                </div>
-            </div>
-        </div>
-        `;
-        productsContainer.innerHTML += cardHtml;
-    });
-}
 
     // 멍멍이 버튼 클릭 이벤트 리스너
     document.querySelector("input#btnradio1").addEventListener("click", function() {
