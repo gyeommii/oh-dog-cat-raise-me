eaed2e6ba05af6a004abb44a0179c89e142adb52
tlacodnjs667 <tlacodnjs667@gmail.com>
2023-12-21 13:28:56 +0900

    feat:ImgRestApiController 구현

--- src/main/java/com/ohdogcat/web/ImageRestController.java
+++ src/main/java/com/ohdogcat/web/ImageRestController.java
@@ -1,8 +1,10 @@
 package com.ohdogcat.web;
 
 import com.ohdogcat.util.FtpImgLoaderUtil;
+import jakarta.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/image")
@@ -19,8 +22,11 @@ public class ImageRestController {
 
     private final FtpImgLoaderUtil imgUploader;
 
-    @GetMapping("/")
-    public ResponseEntity<Resource> getImage(@RequestParam String imgUrl) throws IOException {
+    @GetMapping("")
+    public ResponseEntity<Resource> getImage(@RequestParam String imgUrl, HttpServletRequest req)
+        throws IOException {
+        log.debug("getImage(imgUrl={}", imgUrl);
+
         Resource resource = imgUploader.download(imgUrl);
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
