e20656913de00bcf00cc4be67dce666aaba4ab0f
unknown <minkyu45@naver.com>
2023-12-27 11:51:27 +0900

    branch switch

--- src/main/java/com/ohdogcat/dto/member/pet/PetListDto.java
+++ src/main/java/com/ohdogcat/dto/member/pet/PetListDto.java
@@ -1,6 +1,5 @@
 package com.ohdogcat.dto.member.pet;
 
-import org.springframework.web.multipart.MultipartFile;
 import com.ohdogcat.model.Pet;
 import lombok.Builder;
 import lombok.Data;
@@ -10,26 +9,18 @@ import lombok.Data;
 public class PetListDto {
   private long pet_pk;
   private long member_fk;
-  private String img_url;
+  private String img;
   private String pet_name;
   private String pet_type;
   private String age;
   private String gender;
   private String chehyeong;
-  
+
   public static PetListDto fromEntity(Pet pet) {
-    
-    return PetListDto.builder()
-        .pet_pk(pet.getPet_pk())
-        .member_fk(pet.getMember_fk())
-        .img_url(pet.getImg_url())
-        .pet_name(pet.getPet_name())
-        .pet_type(pet.getPet_type())
-        .age(pet.getAge())
-        .gender(pet.getGender())
-        .chehyeong(pet.getChehyeong())
-        .build();
-        
+
+    return PetListDto.builder().pet_pk(pet.getPet_pk()).member_fk(pet.getMember_fk())
+        .img(pet.getImg()).pet_name(pet.getPet_name()).pet_type(pet.getPet_type()).age(pet.getAge())
+        .gender(pet.getGender()).chehyeong(pet.getChehyeong()).build();
   }
 
 }
--- src/main/java/com/ohdogcat/model/Pet.java
+++ src/main/java/com/ohdogcat/model/Pet.java
@@ -1,6 +1,5 @@
 package com.ohdogcat.model;
 
-import org.springframework.web.multipart.MultipartFile;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,12 +12,11 @@ import lombok.NoArgsConstructor;
 public class Pet {
   private long pet_pk;
   private long member_fk;
-  private MultipartFile img;
-  private String img_url;
+  private String img;
   private String pet_name;
   private String pet_type;
   private String age;
   private String gender;
   private String chehyeong;
-  
+
 }
